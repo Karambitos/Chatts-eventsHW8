5a59cea9f04f4a9b1ea3584b71164b7c6acef4c6
KarambitoS <pase4nikdv@gmail.com>
2020-11-20 11:19:16 +0200

    last test

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ import items from "././gallery-items.js";
 const galleryList = document.querySelector(".js-gallery");
 const lightbox = document.querySelector(".js-lightbox");
 const lightboxImg = lightbox.querySelector("img");
-const lightboxBtn = lightbox.querySelector("button[data-action='close-lightbox']");
-console.log(lightboxBtn);
-
+const lightboxBtn = lightbox.querySelector(
+  "button[data-action='close-lightbox']"
+);
 const galleryItems = items
-    .map(
-        (items) =>
-        `<li class="gallery__item">
+  .map(
+    (items) =>
+      `<li class="gallery__item">
             <a
                 class="gallery__link"
                 href = "${items.original}"
@@ -22,52 +22,46 @@ const galleryItems = items
                 />
             </a>
             </li>`
-    )
-    .join("");
-galleryList.insertAdjacentHTML('beforeend', galleryItems);
+  )
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", galleryItems);
 
-galleryList.addEventListener('click', respondToTheClick);
-lightboxBtn.addEventListener('click', closeLightbox);
+galleryList.addEventListener("click", respondToTheClick);
+lightboxBtn.addEventListener("click", closeLightbox);
 
-
-window.addEventListener('keydown', (event) => {
-    let turgetCode = event.code
-    switch (turgetCode) {
-        case 'Escape':
-            closeLightbox();
-            break;
-        case 'ArrowRight':
-            console.log(turgetCode);
-            break;
-        case 'ArrowLeft':
-            console.log(turgetCode);
-            break;
-    }
-})
+window.addEventListener("keydown", (event) => {
+  let turgetCode = event.code;
+  switch (turgetCode) {
+    case "Escape":
+      closeLightbox();
+      break;
+    case "ArrowRight":
+      console.log(turgetCode);
+      break;
+    case "ArrowLeft":
+      console.log(turgetCode);
+      break;
+  }
+});
 
 function respondToTheClick(event) {
-    event.preventDefault();
-    const target = event.target;
-    if (target.nodeName !== 'IMG') return
-    const targetData = target.dataset.source;
-    openLightbox();
-    addLightboxSrc(targetData);
-    // nextImage(event);
+  event.preventDefault();
+  const target = event.target;
+  if (target.nodeName !== "IMG") return;
+  const targetData = target.dataset.source;
+  openLightbox();
+  addLightboxSrc(targetData);
 }
 
 function openLightbox() {
-    lightbox.classList.add('is-open');
+  lightbox.classList.add("is-open");
 }
 
 function closeLightbox() {
-    lightbox.classList.remove('is-open');
-    addLightboxSrc();
+  lightbox.classList.remove("is-open");
+  addLightboxSrc();
 }
 
 function addLightboxSrc(currentData) {
-    currentData ? lightboxImg.src = currentData : lightboxImg.src = "";
+  currentData ? (lightboxImg.src = currentData) : (lightboxImg.src = "");
 }
-
-// function nextImage(event) {
-//     console.log('nextImage', event.currentTarget);
-// }
